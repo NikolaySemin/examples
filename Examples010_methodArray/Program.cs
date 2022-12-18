60ec6b2c763dbe70aaa739685bd5d272c4b5df3e
SeminNikolay <Nik.urist@mail.ru>
2022-12-18 11:27:28 +0300

    example010 искать число

--- Examples010_methodArray/Program.cs
+++ Examples010_methodArray/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 17, 26, 43, 8, 4, 55, 6, 73, 8 };
+
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+
+    index++;
+}
